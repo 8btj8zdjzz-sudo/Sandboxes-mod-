5aaa046130a0d7f1230810bb5cfdb28b6b30307b
8btj8zdjzz-sudo <8btj8zdjzz@privaterelay.appleid.com>
2026-07-29 10:05:11 -0400

    Update alchemy_unlock.js

--- alchemy_unlock.js
+++ alchemy_unlock.js
@@ -1,12 +1,6 @@
-// Unlock Sand only
-
-if (elements.sand) {
-    elements.sand.hidden = false;
-    elements.sand.category = "powders";
-}
-
 runAfterLoad(function() {
-    if (typeof createElementButton === "function") {
+    if (elements.sand) {
+        elements.sand.hidden = false;
         createElementButton("sand");
     }
 });
