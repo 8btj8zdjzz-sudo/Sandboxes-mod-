f629f7cce79bc848d95e6024ec9e7bcd25d3190c
8btj8zdjzz-sudo <8btj8zdjzz@privaterelay.appleid.com>
2026-07-29 12:17:43 -0400

    Update alchemy_unlock.js

--- alchemy_unlock.js
+++ alchemy_unlock.js
@@ -1,6 +1,15 @@
 runAfterLoad(function() {
+    settings.unlocked = settings.unlocked || {};
+
+    settings.unlocked.sand = true;
+
     if (elements.sand) {
         elements.sand.hidden = false;
+    }
+
+    if (typeof createElementButton === "function") {
         createElementButton("sand");
     }
+
+    saveSettings();
 });
