f46b4a8261744fbed34ab2d5c040ff51f310a9e3
8btj8zdjzz-sudo <8btj8zdjzz@privaterelay.appleid.com>
2026-07-29 12:32:28 -0400

    Update alchemy_unlock.js

--- alchemy_unlock.js
+++ alchemy_unlock.js
@@ -1,7 +1,46 @@
-// Sand unlock
+// Alchemy Mod - Start with basic elements and unlock as discovered
 
-elements.sand.hidden = false;
+if (!settings.alchemyUnlocked) {
+    settings.alchemyUnlocked = {
+        sand: true,
+        dirt: true,
+        water: true,
+        fire: true,
+        oxygen: true
+    };
+}
 
+// Hide locked elements
+for (var element in elements) {
+
+    if (settings.alchemyUnlocked[element]) {
+        elements[element].hidden = false;
+    }
+    else if (elements[element].category !== "tools") {
+        elements[element].hidden = true;
+    }
+}
+
+// Unlock elements when they are created
 runAfterLoad(function() {
-    createElementButton("sand");
+
+    let oldCreatePixel = createPixel;
+
+    createPixel = function(element, x, y) {
+
+        if (elements[element] && !settings.alchemyUnlocked[element]) {
+
+            settings.alchemyUnlocked[element] = true;
+            elements[element].hidden = false;
+
+            if (typeof createElementButton === "function") {
+                createElementButton(element);
+            }
+
+            saveSettings();
+        }
+
+        return oldCreatePixel(element, x, y);
+    };
+
 });
